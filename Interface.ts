a64b7ddd9104f13c31c4c49a6df964062e4154f2
RakibRahman <md.rr.talukder@gmail.com>
2021-09-24 15:48:17 +0600

    Intro to interface

--- Interface.ts
+++ Interface.ts
@@ -4,3 +4,32 @@ type Menu = {
 };
 
 let menu: Menu;
+
+const createMenu = (name: string, links: string[]): Menu => {
+  return {
+    name,
+    links,
+  };
+};
+
+menu = createMenu("Nav", ["home", "about"]);
+console.log(menu);
+
+//! Interface
+// interface is recommended for complex data structures or data set
+//Defines the shape or structure of a object
+interface Pizza {
+  name: string;
+  sizes: string[];
+}
+
+let foodItem: Pizza;
+
+const createFoodItem = (name: string, sizes: string[]): Pizza => {
+  return {
+    name,
+    sizes,
+  };
+};
+foodItem = createFoodItem("Birayani", ["eggs", "rice"]);
+console.log(foodItem);
