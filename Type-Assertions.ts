1a31a8340787694913d679ef67156fd1e5776a40
RakibRahman <md.rr.talukder@gmail.com>
2021-09-26 21:06:39 +0600

    example updated

--- Type-Assertions.ts
+++ Type-Assertions.ts
@@ -6,13 +6,13 @@ const user2: User = { name: "zakir", id: 24 };
 const userInfo: CallbackFn = (r) => `${r.name}`;
 const res1 = userInfo(user1);
 const res2 = userInfo(user2);
-console.log(res1);
-console.log(res2);
+// console.log(res1);
+// console.log(res2);
 
 //! Type Assertion
 let code: any = 123;
 let userCode = <number>code;
-console.log(typeof userCode);
+// console.log(typeof userCode);
 
 // let employee = {};
 // employee.name = "pain";  will give compiler error
@@ -21,7 +21,7 @@ let employee = <User>{};
 employee.name = "sakib";
 employee.id = 89;
 
-console.log(employee);
+// console.log(employee);
 
 const serialized = JSON.stringify(user1);
 
@@ -29,4 +29,19 @@ const getInfoFromJSON = (obj: string) => {
   return JSON.parse(obj) as User;
 };
 const user1Name = getInfoFromJSON(serialized).name;
-console.log(user1Name);
+// console.log(user1Name);
+
+//Demo
+
+const calculateVat = (amount: number, format: boolean): string | number => {
+  const calcAmount = amount * 1.2;
+  return format ? `${calcAmount.toFixed(2)}` : calcAmount;
+};
+
+//type Assertion
+let vatNumber = calculateVat(500, false) as number;
+let vatString = calculateVat(300, true) as string;
+console.log(`Number vat: ${vatNumber}`);
+console.log(`String vat: ${vatString}`);
+
+// let taxBoolean = calculateVat(100, false) as boolean;
