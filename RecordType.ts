efc3c686fc6b76e60d5f44e6fdf165c8aa0372de
RakibRahman <md.rr.talukder@gmail.com>
2022-11-17 08:21:59 +0600

    Record example updated

--- RecordType.ts
+++ RecordType.ts
@@ -35,3 +35,23 @@ const features: FeatureToggles = Object.freeze({
 //! Check for an empty object
 
 type IsEmpty<T> = T extends Record<string, never> ? true : false;
+
+type Sources = 'salary' | 'bonus' | 'blogging'
+
+type Incomes = Record<Sources, number>
+
+const totalIncome = (salary: Incomes): number => {
+  let total = 0;
+  for (let m in salary) {
+
+    total += salary[m as keyof Incomes]
+  }
+  return total;
+
+}
+
+console.log(totalIncome({
+  salary: 27000,
+  bonus: 0,
+  blogging: 0
+}));
